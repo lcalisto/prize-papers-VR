c44f22b187542a53bc53b894c3f0cb4eb1a3e426
Luis Calisto <luis.calisto@hotmail.com>
2018-07-27 19:07:54 +0100

    o

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 function onFilterBtn(){
-	var ctlMapSettingsBtn=document.getElementById('ctlMapSettingsBtn');
-	ctlMapSettingsBtn.components['gui-button'].setActiveState(false);
-	var ctlFilterBtn=document.getElementById('ctlFilterBtn');
-	ctlFilterBtn.components['gui-button'].setActiveState(true);
+//	var ctlMapSettingsBtn=document.getElementById('ctlMapSettingsBtn');
+//	ctlMapSettingsBtn.components['gui-button'].setActiveState(false);
+//	var ctlFilterBtn=document.getElementById('ctlFilterBtn');
+//	ctlFilterBtn.components['gui-button'].setActiveState(true);
 }
 
 function onMapSettingsBtn(){
-	var ctlFilterBtn=document.getElementById('ctlFilterBtn');
-	ctlFilterBtn.components['gui-button'].setActiveState(false);
-	var ctlMapSettingsBtn=document.getElementById('ctlMapSettingsBtn');
-	ctlMapSettingsBtn.components['gui-button'].setActiveState(true);
+//	var ctlFilterBtn=document.getElementById('ctlFilterBtn');
+//	ctlFilterBtn.components['gui-button'].setActiveState(false);
+//	var ctlMapSettingsBtn=document.getElementById('ctlMapSettingsBtn');
+//	ctlMapSettingsBtn.components['gui-button'].setActiveState(true);
 }
